61e665edd311fdd5888b5678e179c3f52a1ebc9b
ROKUMATE <rohitkumawat0110@gmail.com>
2025-08-05 13:28:02 +0530

    build changes

--- src/Components/Home/Gallery/Gallery.tsx
+++ src/Components/Home/Gallery/Gallery.tsx
@@ -1,73 +1,72 @@
-import React from "react";
-import Masonry from "@/Components/ui/Masonry";
+import Masonry from '@/Components/ui/Masonry';
 
 const Gallery = () => {
   const items = [
     {
-      id: "1",
-      img: "/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG",
+      id: '1',
+      img: '/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG',
       height: 400,
     },
     {
-      id: "2",
-      img: "/assets/Images/Home/Gallery/EventImages/Code_rush.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Code_rush.JPG",
+      id: '2',
+      img: '/assets/Images/Home/Gallery/EventImages/Code_rush.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Code_rush.JPG',
       height: 250,
     },
     {
-      id: "3",
-      img: "/assets/Images/Home/Gallery/EventImages/Hackatron.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Hackatron.JPG",
+      id: '3',
+      img: '/assets/Images/Home/Gallery/EventImages/Hackatron.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Hackatron.JPG',
       height: 600,
     },
 
     {
-      id: "4",
-      img: "/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG",
+      id: '4',
+      img: '/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG',
       height: 600,
     },
 
     {
-      id: "5",
-      img: "/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG",
+      id: '5',
+      img: '/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG',
       height: 600,
     },
 
     {
-      id: "6",
-      img: "/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG",
+      id: '6',
+      img: '/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG',
       height: 600,
     },
 
     {
-      id: "7",
-      img: "/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG",
+      id: '7',
+      img: '/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG',
       height: 600,
     },
 
     {
-      id: "8",
-      img: "/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG",
+      id: '8',
+      img: '/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG',
       height: 600,
     },
 
     {
-      id: "9",
-      img: "/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG",
+      id: '9',
+      img: '/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG',
       height: 600,
     },
 
     {
-      id: "10",
-      img: "/assets/Images/Home/Gallery/EventImages/Robowars.JPG",
-      url: "/assets/Images/Home/Gallery/EventImages/Robowars.JPG",
+      id: '10',
+      img: '/assets/Images/Home/Gallery/EventImages/Robowars.JPG',
+      url: '/assets/Images/Home/Gallery/EventImages/Robowars.JPG',
       height: 600,
     },
   ];
@@ -75,11 +74,10 @@ const Gallery = () => {
     <div
       className="w-full h-dvh max-h-[800px] relative bg-stone-950 overflow-hidden flex items-center justify-center p-6 overflow-y-scroll"
       style={{
-        overflow: "scroll",
-        scrollbarWidth: "none", // Firefox
-        msOverflowStyle: "none", // IE 10+
-      }}
-    >
+        overflow: 'scroll',
+        scrollbarWidth: 'none', // Firefox
+        msOverflowStyle: 'none', // IE 10+
+      }}>
       <Masonry
         items={items}
         ease="power3.out"
