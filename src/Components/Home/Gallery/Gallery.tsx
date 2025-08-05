092656127c701b18e27d5d7a3f2f5aea290cf0a5
ROKUMATE <rohitkumawat0110@gmail.com>
2025-08-05 23:37:36 +0530

    updated the gallery images

--- src/Components/Home/Gallery/Gallery.tsx
+++ src/Components/Home/Gallery/Gallery.tsx
@@ -4,69 +4,69 @@ const Gallery = () => {
   const items = [
     {
       id: '1',
-      img: '/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Closing_ceremony.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416889/Closing_ceremony_Large_jeorbx.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416889/Closing_ceremony_Large_jeorbx.jpg',
       height: 400,
     },
     {
       id: '2',
-      img: '/assets/Images/Home/Gallery/EventImages/Code_rush.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Code_rush.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Code_rush_xjqlbp.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Code_rush_xjqlbp.jpg',
       height: 250,
     },
     {
       id: '3',
-      img: '/assets/Images/Home/Gallery/EventImages/Hackatron.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Hackatron.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416867/Hackatron_nyff3m.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416867/Hackatron_nyff3m.jpg',
       height: 600,
     },
 
     {
       id: '4',
-      img: '/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Pinnacle.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416642/Pinnacle_kj93pf.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416642/Pinnacle_kj93pf.jpg',
       height: 600,
     },
 
     {
       id: '5',
-      img: '/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Pronite_1.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416853/Pronite_1_Large_gmdiqd.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416853/Pronite_1_Large_gmdiqd.jpg',
       height: 600,
     },
 
     {
       id: '6',
-      img: '/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Pronite_2.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_2_h8teb8.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_2_h8teb8.jpg',
       height: 600,
     },
 
     {
       id: '7',
-      img: '/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Pronite_3.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_3_c3aqmj.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_3_c3aqmj.jpg',
       height: 600,
     },
 
     {
       id: '8',
-      img: '/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Pronite_4.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_4_uqah6u.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416640/Pronite_4_uqah6u.jpg',
       height: 600,
     },
 
     {
       id: '9',
-      img: '/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/RoboMaze.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416642/RoboMaze_jznorw.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416642/RoboMaze_jznorw.jpg',
       height: 600,
     },
 
     {
       id: '10',
-      img: '/assets/Images/Home/Gallery/EventImages/Robowars.JPG',
-      url: '/assets/Images/Home/Gallery/EventImages/Robowars.JPG',
+      img: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416844/Robowars_Large_swnqbv.jpg',
+      url: 'https://res.cloudinary.com/dih6gwkau/image/upload/v1754416844/Robowars_Large_swnqbv.jpg',
       height: 600,
     },
   ];
