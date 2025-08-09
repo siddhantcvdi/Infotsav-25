73de5b8d4398a13c8e51606b8e0088284a1584aa
ROKUMATE <rohitkumawat0110@gmail.com>
2025-08-09 12:27:46 +0530

    edited the mobile responsiveness sponsers section

--- src/Components/ui/HangingSponsor.tsx
+++ src/Components/ui/HangingSponsor.tsx
@@ -1,10 +1,23 @@
 interface HangingSponsorProps {
     imageURL?: string;
+    isMobile?: boolean;
 }
 
-const HangingSponsor = ({ imageURL }: HangingSponsorProps) => {
+const HangingSponsor = ({
+    imageURL,
+    isMobile = false,
+}: HangingSponsorProps) => {
     return (
-        <div className="w-[11%] max-sm:w-[28%] max-md:w-[20%] aspect-square bg-white/10 border-white/20 border-[3px] rounded-full overflow-hidden flex items-center justify-center">
+        <div
+            className={`${
+                isMobile
+                    ? 'w-36 h-36 sm:w-40 sm:h-40'
+                    : 'w-[11%] max-sm:w-[28%] max-md:w-[20%]'
+            } ${
+                !isMobile ? 'aspect-square' : ''
+            } bg-white/10 border-white/20 border-[3px] rounded-full overflow-hidden flex items-center justify-center ${
+                isMobile ? 'p-1' : 'p-3'
+            }`}>
             {imageURL && (
                 <img
                     src={imageURL}
